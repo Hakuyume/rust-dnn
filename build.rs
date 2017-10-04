d0ee7c8ad29c6a463fc894d4b748fa0a2c8a9132
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-04 09:32:50 +0900

    use nvcc

--- build.rs
+++ build.rs
@@ -1,20 +1,5 @@
-use std::env;
-use std::path;
-use std::process;
+extern crate nvcc;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
-    process::Command::new("nvcc")
-        .args(&["-c", "src/custom_kernel.cu", "-Xcompiler", "-fPIC", "-o"])
-        .arg(&format!("{}/custom_kernel.o", out_dir))
-        .status()
-        .unwrap();
-    process::Command::new("ar")
-        .args(&["crus", "libcustom_kernel.a", "custom_kernel.o"])
-        .current_dir(&path::Path::new(&out_dir))
-        .status()
-        .unwrap();
-
-    println!("cargo:rustc-link-search={}", out_dir);
+    nvcc::compile_library("libcustom_kernel.a", &["src/custom_kernel.cu"]);
 }
