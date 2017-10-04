6217c4e8f58b94ab5fdb2e32b7f3eb6a07a82d49
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-04 12:35:15 +0900

    use rand

--- examples/relu.rs
+++ examples/relu.rs
@@ -1,3 +1,6 @@
+extern crate rand;
+use rand::Rng;
+
 extern crate cuda;
 extern crate cudnn;
 extern crate nn;
@@ -7,10 +10,12 @@ use nn::custom;
 
 fn main() {
     let mut x = memory::Memory::new(16).unwrap();
-    memory::memcpy(&mut x,
-                   &[0.0, 0.1, -0.2, -0.3, 0.4, -0.5, 0.6, -0.7, 0.8, 0.9, 1.0, -1.1, 1.2, -1.3,
-                     1.4, 1.5])
-            .unwrap();
+    {
+        let mut rng = rand::thread_rng();
+        for x in x.iter_mut() {
+            *x = rng.gen::<f32>();
+        }
+    }
 
     {
         let mut host = vec![0.; x.len()];
