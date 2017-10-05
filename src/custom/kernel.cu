ea228ad7e5039984dc0f9723a400690dc728765f
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-05 19:52:35 +0900

    src -> x

--- src/custom/kernel.cu
+++ src/custom/kernel.cu
@@ -1,9 +1,9 @@
 #define FOREACH(index, length) for (size_t index = threadIdx.x + blockIdx.x * blockDim.x; index < length; index += gridDim.x * blockDim.x)
 
-extern "C" __global__ void relu_forward_inplace_f(float *src, size_t len)
+extern "C" __global__ void relu_forward_inplace_f(float *x, size_t len)
 {
   FOREACH(i, len) {
-    if (src[i] < 0)
-      src[i] = 0;
+    if (x[i] < 0)
+      x[i] = 0;
   }
 }
