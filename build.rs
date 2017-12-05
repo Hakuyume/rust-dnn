38ece2a132fc2dd59c8d98c6d1adb724b983f1de
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-05 10:54:21 +0900

    generate using build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,29 @@
 extern crate nvcc;
 
+use std::fs;
+
+use std::io::Write;
+
 fn main() {
     nvcc::compile_library("libcustom_kernel.a", &["src/kernel.cu"]);
+
+    {
+        let mut file = fs::File::create("src/generic_value/values.rs").unwrap();
+        for i in 0..1024 {
+            file.write_fmt(format_args!("pub struct U{};\n", i))
+                .unwrap();
+        }
+    }
+
+    {
+        let mut file = fs::File::create("src/generic_value/impls.rs").unwrap();
+        file.write_all(b"use super::traits::USize;\n").unwrap();
+        file.write_all(b"use super::values::*;\n").unwrap();
+        for i in 0..1024 {
+            file.write_fmt(format_args!("impl USize for U{} {{ const VALUE: usize = {}; }}\n",
+                                        i,
+                                        i))
+                .unwrap();
+        }
+    }
 }
--- src/generic_value/values.rs
+++
@@ -1,5 +0,0 @@
-pub struct U0;
-pub struct U1;
-pub struct U10;
-pub struct U32;
-pub struct U784;
