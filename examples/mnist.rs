439fcee88af27c495f5bb76af3d3d4b620bb8a65
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-13 15:42:50 +0900

    randomly sample

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -1,5 +1,5 @@
+extern crate rand;
 extern crate cuda;
-extern crate cudnn;
 extern crate nn;
 
 use nn::dataset::MNIST;
@@ -14,6 +14,7 @@ type NClasses = U10;
 
 fn main() {
     let mut context = nn::Context::new().unwrap();
+    let mut rng = rand::thread_rng();
 
     let mut x = nn::Tensor::<_, BatchSize, InputSize, _, _>::new().unwrap();
     let mut t = nn::Tensor::<_, _, NClasses, _, _>::new().unwrap();
@@ -26,21 +27,23 @@ fn main() {
     let mut softmax_cross_entropy = nn::layer::SoftmaxCrossEntropy::new().unwrap();
 
     let mnist = MNIST::new("mnist").unwrap();
-    {
-        let mut x_host = vec![0.; x.mem().len()];
-        let mut t_host = vec![0.; t.mem().len()];
-        for i in 0..BatchSize::VALUE {
-            let (image, label) = mnist.train.get(i);
-            for k in 0..InputSize::VALUE {
-                x_host[i * InputSize::VALUE + k] = image[k] as f32;
-                t_host[i * NClasses::VALUE + label as usize] = 1.;
-            }
-        }
-        cuda::memory::memcpy(x.mem_mut(), &x_host).unwrap();
-        cuda::memory::memcpy(t.mem_mut(), &t_host).unwrap();
-    }
 
     for _ in 0..50 {
+        {
+            let indices = rand::seq::sample_indices(&mut rng, mnist.train.len(), BatchSize::VALUE);
+            let mut x_host = vec![0.; x.mem().len()];
+            let mut t_host = vec![0.; t.mem().len()];
+            for (i, index) in indices.into_iter().enumerate() {
+                let (image, label) = mnist.train.get(index);
+                for k in 0..InputSize::VALUE {
+                    x_host[i * InputSize::VALUE + k] = image[k] as f32;
+                    t_host[i * NClasses::VALUE + label as usize] = 1.;
+                }
+            }
+            cuda::memory::memcpy(x.mem_mut(), &x_host).unwrap();
+            cuda::memory::memcpy(t.mem_mut(), &t_host).unwrap();
+        }
+
         fc.forward(&mut context, &x, &mut y).unwrap();
         let loss = softmax_cross_entropy
             .compute(&mut context, &y, &t, &mut dy)
